c0b2e5ee67310ae5eb29720158a4fed7fde6028d
maure <mauricioknob@gmail.com>
2026-06-07 13:18:26 -0300

    feat(ioc): register Redis distributed cache and ISaleCacheService in DI

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Common;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
 using Microsoft.AspNetCore.Builder;
@@ -23,5 +24,13 @@ public class InfrastructureModuleInitializer : IModuleInitializer
         builder.Services.AddScoped<IMongoDatabase>(sp =>
             sp.GetRequiredService<IMongoClient>().GetDatabase(mongoDatabaseName));
         builder.Services.AddScoped<IMongoSaleRepository, MongoSaleRepository>();
+
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration["Redis:ConnectionString"];
+            options.InstanceName = "ambev_";
+        });
+
+        builder.Services.AddScoped<ISaleCacheService, RedisSaleCacheService>();
     }
 }
